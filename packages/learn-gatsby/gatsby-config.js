93c86c0c0df140a332ac10f991959af2dbff40b2
Pavel Lasarev <Pavel_Lasarev@epam.com>
2021-03-26 13:18:21 +0400

    add learn-gatsby pwa project

---
+++ packages/learn-gatsby/gatsby-config.js
@@ -0,0 +1,55 @@
+module.exports = {
+  siteMetadata: {
+    title: `Gatsby Redux-Toolkit`,
+    description: `Gatsby, Redux-Toolkit, Typescript, Styled Components & Tailwind CSS starter`,
+    author: `Saimir Kapaj`
+  },
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/assets/images`
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-redux-toolkit-typescript-starter`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#667EEA`,
+        theme_color: `#667EEA`,
+        display: `minimal-ui`,
+        icon: `src/assets/images/gatsby-icon.png`
+      }
+    },
+    `gatsby-plugin-styled-components`,
+    `gatsby-plugin-postcss`,
+    `gatsby-plugin-tailwindcss`,
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        tailwind: true,
+        purgeOnly: [`src/assets/styles/global.css`]
+      }
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `learn-gatsby`,
+        short_name: `learn-gatsby`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/assets/images/gatsby-icon.png`,
+      },
+    },
+    `gatsby-plugin-offline`,
+  ]
+};
